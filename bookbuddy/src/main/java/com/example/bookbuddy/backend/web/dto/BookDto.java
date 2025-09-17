de6f2d9df3878cdd71f91cc964672e2b8ac99df7
Ben Smith <smithb1900@gmail.com>
2025-09-17 01:30:39 -0400

    final update before deployment

--- bookbuddy/src/main/java/com/example/bookbuddy/backend/web/dto/BookDto.java
+++ bookbuddy/src/main/java/com/example/bookbuddy/backend/web/dto/BookDto.java
@@ -2,41 +2,25 @@ package com.example.bookbuddy.backend.web.dto;
 
 public class BookDto {
 
-<<<<<<< Updated upstream
     private String isbn;
-=======
-    private Long isbn;
->>>>>>> Stashed changes
     private String bookname;
     private String author;
     private String genre;
 
     public BookDto() {}
 
-<<<<<<< Updated upstream
     public BookDto(String isbn, String bookname, String author, String genre) {
-=======
-    public BookDto(Long isbn, String bookname, String author, String genre) {
->>>>>>> Stashed changes
         this.isbn = isbn;
         this.bookname = bookname;
         this.author = author;
         this.genre = genre;
     }
 
-<<<<<<< Updated upstream
     public String getIsbn() {
         return isbn;
     }
 
     public void setIsbn(String isbn) {
-=======
-    public Long getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(Long isbn) {
->>>>>>> Stashed changes
         this.isbn = isbn;
     }
 
