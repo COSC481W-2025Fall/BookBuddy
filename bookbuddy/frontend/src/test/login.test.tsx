ea88d452ef6d1de603f2cd10a52654807454fbe5
rretan <196845036+rretan@users.noreply.github.com>
2025-09-23 15:21:29 -0400

    Messed around with testing
    Created and wrote my front end test in login.test.tsx

---
+++ bookbuddy/frontend/src/test/login.test.tsx
@@ -0,0 +1,27 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { Routes, Route } from "react-router-dom";
+import { MemoryRouter } from 'react-router-dom'
+import Login from './login';
+
+
+//import { cleanup } from '@testing-library/react';
+
+//import matchers from '@testing-library/jest-dom/matchers';
+
+//expect.extend(matchers);
+
+
+describe('Login', () => {
+    it('should show a login screen', () => {
+        render(<MemoryRouter><Login /></MemoryRouter>);
+        expect(screen.getAllByText('Login'));
+    });
+})
+
+/*
+//cleans up doms if affected during testing
+afterEach(() => {
+  cleanup();
+});
+*/
