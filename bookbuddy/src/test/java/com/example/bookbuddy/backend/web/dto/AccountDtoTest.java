062101da2b3aeef5733228cde4efd959c0a82432
rretan <196845036+rretan@users.noreply.github.com>
2025-09-26 16:10:12 -0400

    3 new java tests for the mappers
    old test changed (I dont know what is different)

--- bookbuddy/src/test/java/com/example/bookbuddy/backend/web/dto/AccountDtoTest.java
+++ bookbuddy/src/test/java/com/example/bookbuddy/backend/web/dto/AccountDtoTest.java
@@ -1,4 +1,14 @@
+package com.example.bookbuddy.backend.web.dto;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AccountDtoTest {
-  
+    //Test of AccountDto constructor
+    @Test
+    public void testConstructor() {
+        AccountDto accountDto = new AccountDto();
+        assertNotNull(accountDto);
+    }
 }
