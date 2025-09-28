48aa5653cb014c758436e864c72ee989585a1128
rretan <196845036+rretan@users.noreply.github.com>
2025-09-28 15:26:27 -0400

    Updated pom and vite.config to get this branch to work like main

--- bookbuddy/frontend/vite.config.ts
+++ bookbuddy/frontend/vite.config.ts
@@ -8,15 +8,36 @@ export default defineConfig({
     emptyOutDir: true,
     assetsDir: '', // optional: keep assets flat for simplicity
   },
-  server: {
-    proxy: {
-      '/Account': 'http://localhost:8080',
-        '/Book': 'http://localhost:8080',
-        '/login': 'http://localhost:8080',
-        '/openlib': 'http://localhost:8080',
-    }
-  },
-  test: {
+    server: {
+        proxy: {
+            '/Account': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+            },
+            '/Book': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+            },
+            '/login': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+            },
+            '/googlebooks': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+            },
+            '/books': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+            },
+        }
+    },
+    test: {
       environment: 'jsdom',
       globals: true,
       coverage: {
