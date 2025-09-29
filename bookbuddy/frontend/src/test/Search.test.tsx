f9c8aafd39557608375a2de19e96d4b890d57eac
rretan <196845036+rretan@users.noreply.github.com>
2025-09-28 20:40:29 -0400

    Added test for search.tsx inside the test file
    Has an error but does hit around 80%
    
    Added role = "form" to search.tsx for the test functionality

--- bookbuddy/frontend/src/test/Search.test.tsx
+++ bookbuddy/frontend/src/test/Search.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Search from "../Search";
+import { Routes, Route, MemoryRouter, BrowserRouter } from "react-router-dom";
+import { act } from 'react';
+
 
 //Test to see if the route is rendered
 describe('Search Rendering', () => {
@@ -16,4 +18,32 @@ describe('Search Rendering', () => {
         //sees if it exists
         expect(container).toBeInTheDocument();
     });
+});
+
+//test to see if the search works properly
+test('search is returned correctly', () => {
+    afterEach(() => {
+      vi.clearAllMocks()
+    });
+
+    //renders the page
+    render(<MemoryRouter><Search /></MemoryRouter>);
+
+    var form = screen.getByRole('form');
+
+    //input for the title to be searched
+    const titleInput = screen.getByLabelText("Please enter a book you would like to search");
+
+    act(() => {
+        //inputs the title, and presses the search button
+        fireEvent.change(titleInput, { target: { value: 'Hobbit' } });
+        fireEvent.click(screen.getByRole("button", { name: /Search/i }));
+
+        return screen.findByText(/J.R.R. Tolkien/i).then((message) => {
+            expect(message).toBeInTheDocument();
+            expect(search.Search.toHaveBeenCalled());
+        });
+    });
+
+
 });
