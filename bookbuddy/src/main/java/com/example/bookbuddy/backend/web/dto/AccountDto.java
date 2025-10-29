239d8b39a7d35c32539d5ce98c8687c310cb839d
Noah Schaible <nschaibl@emich.edu>
2025-10-29 19:30:40 -0400

    reverted accountdto

--- bookbuddy/src/main/java/com/example/bookbuddy/backend/web/dto/AccountDto.java
+++ bookbuddy/src/main/java/com/example/bookbuddy/backend/web/dto/AccountDto.java
@@ -1,32 +1,7 @@
 package com.example.bookbuddy.backend.web.dto;
 
 public class AccountDto {
-    private Long accountId;
-    private String name;
-    private String password;
-
-    public Long getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(Long accountId) {
-        this.accountId = accountId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public Long accountId;   // Account ID
+    public String name;      // Account Name
+    public String password;  // Account Password
 }
-
