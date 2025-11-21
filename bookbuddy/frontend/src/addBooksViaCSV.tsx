d4914457fdc5fd7fc26172a484d1839e075bae7e
rretan <196845036+rretan@users.noreply.github.com>
2025-11-20 22:51:40 -0500

    Minor updates to the feature that make it very ready to be shipped
    Problems for future me: The text on the add CSV card does not match the seach card.
    
    I broke a Library.text.tsx that fixed
    
    I added a loading page for the CSV with live updates to the books that are being added. I also made the card able to be clicked anywhere for the upload, namely the image.
    
    Big change was having the page now refresh when the books are added, causing them to actually show up right away

--- bookbuddy/frontend/src/addBooksViaCSV.tsx
+++ bookbuddy/frontend/src/addBooksViaCSV.tsx
@@ -1,30 +1,32 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { searchBookViaTitle } from "./searchBookViaTitle";
 import { addCSVBooks } from "./addCSVBooks";
+import "./components/Book_loading.css"
+import tempAddBook from "./logo/tempAddBook.png";
 
 export default function CSVReader() {
     let [columnData, setColumnData] = useState<string[]>([]);
 
     const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const file = event.target.files?.[0];
-      if (file) {
+        const file = event.target.files?.[0];
+        if (file) {
           setFileName(file.name);
-      } else {
+        } else {
           setFileName('No File Chosen!');
           return;
-      }
+        }
 
-      const reader = new FileReader();
-      reader.onload = (e) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
         const text = e.target?.result as string;
         if (!text) return;
 
         const lines = text.split(/\r?\n/).filter((line) => line.trim() !== "");
         const firstColumn = lines.map((line) => line.split(/,(?=(?:[^"]*"[^"]*")*[^"]*$)/)[1]);
-        setColumnData(firstColumn);
-      };
+            setColumnData(firstColumn);
+        };
 
-      reader.readAsText(file);
+        reader.readAsText(file);
     };
 
     const [books, setBooks] = useState<any[]>([]);
@@ -43,6 +45,7 @@ export default function CSVReader() {
 
    const [bookMessages, setBookMessages] = useState<BookMessage[]>([]);
    const [fileName, setFileName] = useState('No File Chosen!');
+   const [isLoading, setIsLoading] = useState<boolean>(false);
 
    useEffect(() => {
        async function processBooks() {
@@ -55,33 +58,46 @@ export default function CSVReader() {
                columnData = columnData.splice(1, columnData.length);
            }
 
-           const messages: string[] = [];
+           let message = "";
 
+           setIsLoading(true); // Start loading
            for (const title of columnData) {
                const titleClean = title.replaceAll("#","")
 
                // 1. Search the book
                const found = await searchBookViaTitle(titleClean, BASE); // gets the first book
                if (!found) {
-                   messages.push({ titleClean, message: "No result found", success: false });
-                   await delay(100); // IMPORTANT: prevents rate limit so google doesn't cry :(
+                   message = ({ titleClean, message: "No result found", success: false });
+                   await delay(25); // IMPORTANT: prevents rate limit so google doesn't cry :(
                    continue;
                }
                 // 2. Add book to backend
                 const result = await addCSVBooks(found, BASE);
 
-                messages.push({
+                message = ({
                     title: found.bookname,
                     message: result.ok ? "Added successfully" : result.message || "Failed",
                     success: result.ok,
                 });
 
-               await delay(100);
-
+               updateUser(message);
+               await delay(1000);
            }
-           messages.push({title: "Done", message: "Added successfully", success: true})
-           // 3. Display final summary
-           setBookMessages(messages);
+
+           await delay(3000);
+           setIsLoading(false); // stop loading
+           window.location.reload(false);
+       }
+
+       function updateUser(message) {
+           const textBox = document.getElementById("userUpdate");
+           if(message.success){
+               textBox.style.color = "green";
+               textBox.innerHTML = message.title + ":   " + message.message
+           } else {
+               textBox.style.color = "Violet";
+               textBox.innerHTML = message.title + ":   " + message.message
+           }
        }
 
        processBooks();
@@ -89,25 +105,64 @@ export default function CSVReader() {
 
 
     return (
-      <div>
-        {/*<h2 className="text-xl font-semibold mb-2">Upload CSV File</h2>*/}
-        <label htmlFor="fileUpload" className="bb-btn">{fileName}</label>
-        <input
-          style={{ display: "none" }}
-          id="fileUpload"
-          type="file"
-          accept=".csv"
-          onChange={handleFileUpload}
-        />
+        <div>
+        <label htmlFor="fileUpload" style={{ cursor: "pointer" }}>
+                            <div className="bb-card__media">
+                                  <img
+                                    src={tempAddBook}
+                                    style={{
+                                      width: "100%",
+                                      height: "100%",
+                                      objectFit: "cover",
+                                      display: "block",
+                                    }}
+                                  />
+                            </div>
+                            <div className="bb-card__body" >
+                                <h2 className="bb-card__title">
+                                    Add your Goodreads™ Library!
+                                </h2>
+                                <br />
 
-        {/*<div  className="space-y-1">
-            {bookMessages.map((b, i) => (
-                <div key={i} style={{ color: b.success ? "green" : "red" }}>
-                    {b.title}: {b.message}
+                                <label htmlFor="fileUpload" className="bb-btn">{fileName}</label>
+                                <input
+                                  style={{ display: "none" }}
+                                  id="fileUpload"
+                                  type="file"
+                                  accept=".csv"
+                                  onChange = {handleFileUpload}
+                                />
+                            </div>
+                            </label>
+
+            {isLoading && (
+            <div style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                display: 'flex',
+
+                justifyContent: 'center',
+                alignItems: 'center',
+                zIndex: 0,
+                color: 'white',
+                fontSize: '20px',
+                textAlign: 'center'}}>
+                <div className="book">
+                   <div className="book__pg-shadow"></div>
+                   <div className="book__pg"></div>
+                   <div className="book__pg book__pg--2"></div>
+                   <div className="book__pg book__pg--3"></div>
+                   <div className="book__pg book__pg--4"></div>
+                   <div className="book__pg book__pg--5"></div>
                 </div>
-            ))}
-        </div> */}
-        <div> <br /> {bookMessages.map((b) => ("Completed"))}</div>
-      </div>
+                <div id="userUpdate" style={{position: 'fixed', marginTop: '170px', fontSize: '25px',}}>
+
+                </div>
+            </div>)}
+        </div>
     );
 }
