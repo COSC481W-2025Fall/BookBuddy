2ef455b641005dc23c6d87d0ac26f912ee59cf7c
Ben Smith <smithb1900@gmail.com>
2025-11-29 03:21:06 -0500

    final fixes

--- bookbuddy/frontend/src/signup.tsx
+++ bookbuddy/frontend/src/signup.tsx
@@ -173,7 +173,7 @@ const Signup: React.FC = () => {
             {/* ---- BACKGROUND & BUTTONS ---- */}
             <div className="flex-grow flex items-center justify-center gradient-background-books relative h-screen p-8 overflow-hidden z-[-2]">
 
-                <div className="absolute inset-0 bg-[rgba(255,255,255,0.8)] z-5"></div>
+                <div className="absolute inset-0 bg-[rgba(255,255,255,0.8)] z-[-1]"></div>
 
                 {/* THESE BUTTONS DO NOT NAVIGATE ANYMORE */}
                 <button
