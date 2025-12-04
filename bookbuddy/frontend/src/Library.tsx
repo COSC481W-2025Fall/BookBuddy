d759c754e0179b19b8fda5b207cccad75277cf6b
rclearyEMU <rcleary3@emich.edu>
2025-12-04 13:39:13 -0500

    Made my deployment branch

--- bookbuddy/frontend/src/Library.tsx
+++ bookbuddy/frontend/src/Library.tsx
@@ -7,7 +7,7 @@ import noCoverFound from "./logo/noCoverFound.png";
 import tempAddBook from "./logo/tempAddBook.png";
 import tempSearchBook from "./logo/tempSearchBook.png";
 import CSVReader from "./addBooksViaCSV";
-import { TrashIcon } from "@heroicons/react/24/solid";
+// import { TrashIcon } from "@heroicons/react/24/solid";
 
 type SortKey = "name" | "author" | "genre";
 type SortDir = "asc" | "desc";
@@ -166,7 +166,7 @@ export default function Library() {
             className="absolute right-2 top-2 z-10 rounded-full bg-white/90 p-2 text-red-600 opacity-0 shadow transition hover:bg-white hover:text-red-700 focus-visible:ring-2 focus-visible:ring-red-500 group-hover:opacity-100 cursor-pointer"
             title="Remove from Library"
           >
-            <TrashIcon className="h-5 w-5" />
+            {/*<TrashIcon className="h-5 w-5" />*/}
           </button>
         )}
 
